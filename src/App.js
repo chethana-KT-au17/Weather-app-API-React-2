d05459cc0e1eedff661c0fece70e87e51bd453a9
chethanakt3006@gmail.com <chethanakt3006@gmail.com>
2021-08-01 00:13:03 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// import Temp from "./component/weather/temp";
+import Template from "./component/weather/template";
+
+function App() {
+  return (
+    <div>
+     <Template/>
+    </div>
+  );
+}
+
+export default App;
